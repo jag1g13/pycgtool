54a2f3cff81ebb58b68fac61777d03a4bda5e600
James Graham <J.A.Graham@soton.ac.uk>
2016-02-10 22:09:59 +0000

    Read in frame from GRO - working on mappings

--- frame.py
+++ frame.py
@@ -1,18 +1,66 @@
 
+class Mapping:
+    def __init__(self):
+        pass
+
+
+def mappings_from_file(file):
+    mappings = {}
+    with open(file) as f:
+        pass
+
 
 class Atom:
-    def __init__(self, name=None, num=None, type=None):
+    def __init__(self, name=None, num=None, type=None, coords=None):
         self.name = name
         self.num = num
         self.type = type
+        self.coords = coords
+
 
 class Bead(Atom):
     def __init__(self, *args, **kwargs):
         super(Bead, self).__init__(**kwargs)
 
-class Frame:
-    def __init__(self):
-        self.atoms = []
 
-    def addAtom(self, atom):
+class Residue:
+    def __init__(self, name=None, num=None):
+        self.atoms = []
+        self.name = name
+        self.num = num
+
+    def add_atom(self, atom):
         self.atoms.append(atom)
+
+
+class Frame:
+    def __init__(self, gro=None):
+        self.residues = []
+        if gro is not None:
+            self._parse_gro(gro)
+
+    def _parse_gro(self, gro):
+        with open(gro) as g:
+            self.name = g.readline()
+            natoms = int(g.readline())
+            i = 0
+            resnum_last = -1
+            for line in g:
+                resnum = int(line[0:5])
+                resname = line[5:10].strip()
+                atomname = line[10:15].strip()
+                coords = float(line[20:28]), float(line[28:36]), float(line[36:44])
+                if resnum != resnum_last:
+                    self.residues.append(Residue(name=resname,
+                                                 num=resnum))
+                    resnum_last = resnum
+                    atnum = 0
+                atom = Atom(name=atomname, num=atnum, coords=coords)
+                self.residues[-1].add_atom(atom)
+                if i >= natoms - 1:
+                    break
+                i += 1
+                atnum += 1
+
+    def add_residue(self, residue):
+        self.residues.append(residue)
--- test_frame.py
+++ test_frame.py
@@ -1,8 +1,14 @@
 import unittest
 
-from frame import Atom
-from frame import Bead
-from frame import Frame
+from frame import *
+
+
+class MappingTest(unittest.TestCase):
+    def test_mapping_create(self):
+        mappings = mappings_from_file("data/water.map")
+        self.assertEqual(1, len(mappings))
+        self.assertEqual(3, len(mappings["SOL"].atoms))
+        self.assertEqual("OW", len(mappings["SOL"].atoms[0].name))
 
 
 class AtomTest(unittest.TestCase):
@@ -21,16 +27,36 @@ class BeadTest(unittest.TestCase):
         self.assertEqual("Type", bead.type)
 
 
+class ResidueTest(unittest.TestCase):
+    def test_residue_create(self):
+        residue = Residue(name="Resname")
+        self.assertEqual("Resname", residue.name)
+
+    def test_residue_add_atoms(self):
+        atom = Atom(name="Name", num=0, type="Type")
+        residue = Residue()
+        residue.add_atom(atom)
+        self.assertEqual(atom, residue.atoms[0])
+        self.assertTrue(atom is residue.atoms[0])
+
+
 class FrameTest(unittest.TestCase):
     def test_frame_create(self):
         frame = Frame()
 
-    def test_frame_add_atoms(self):
-        atom = Atom(name="Name", num=0, type="Type")
+    def test_frame_add_residue(self):
+        residue = Residue()
         frame = Frame()
-        frame.addAtom(atom)
-        self.assertEqual(atom, frame.atoms[0])
-        self.assertTrue(atom is frame.atoms[0])
+        frame.add_residue(residue)
+        self.assertEqual(residue, frame.residues[0])
+        self.assertTrue(residue is frame.residues[0])
+
+    def test_frame_read_gro(self):
+        frame = Frame(gro="data/water.gro")
+        self.assertEqual(4074, len(frame.residues))
+        self.assertEqual("SOL", frame.residues[0].name)
+        self.assertEqual(3, len(frame.residues[0].atoms))
+        self.assertEqual("OW", frame.residues[0].atoms[0].name)
 
 
 if __name__ == '__main__':
