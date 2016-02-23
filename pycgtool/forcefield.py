894b024aad0bb7aed94a7d77d2a2adecaf0174a8
James Graham <J.A.Graham@soton.ac.uk>
2016-02-23 18:21:41 +0000

    Broken test in Py3.2 part3

--- pycgtool/forcefield.py
+++ pycgtool/forcefield.py
@@ -16,6 +16,9 @@ class ForceField:
         except FileExistsError as e:
             if not os.path.isdir(name):
                 raise e
+        except OSError as e:
+            if e.errno != 17 or not os.path.isdir(name):
+                raise e
 
     def write_rtp(self, mapping, bonds):
         pass
