7a217690622c04d27c5b549af8d4a11bd4f697f4
James Graham <J.A.Graham@soton.ac.uk>
2016-03-01 03:17:27 +0000

    Functions to calculate all angle and dihedrals First steps toward issue #4

--- pycgtool/util.py
+++ pycgtool/util.py
@@ -6,6 +6,91 @@ import numpy as np
 np.seterr(all="raise")
 
 
+def tuple_equivalent(tuple1, tuple2):
+    """
+    Check if two node tuples are equivalent. Assumes undirected edges.
+
+    :param tuple1: First tuple to compare
+    :param tuple2: Second tuple to compare
+    :return: True if tuples are equivalent, else False
+    """
+    if tuple1 == tuple2:
+        return True
+    elif tuple1[::-1] == tuple2:
+        return True
+    else:
+        return False
+
+
+def triplets_from_pairs(nodes, pairs):
+    """
+    Find all connected triplets from a list of connected pairs.
+    Currently uses a naive algorithm, will be optimised if it turns out to be a performance bottleneck.
+
+    :param nodes: List of node names
+    :param pairs: List of node connectivity pairs
+    :return: List of node connectivity triplets
+    """
+    triplets = []
+
+    for node1 in nodes:
+        for node2 in nodes:
+            pair1 = (node1, node2)
+            if pair1 not in pairs and pair1[::-1] not in pairs:
+                continue
+
+            for node3 in nodes:
+                if node3 == node1:
+                    continue
+                pair2 = (node2, node3)
+                if pair2 not in pairs and pair2[::-1] not in pairs:
+                    continue
+
+                triplet = (node1, node2, node3)
+                if triplet not in triplets and triplet[::-1] not in triplets:
+                    triplets.append(triplet)
+
+    return triplets
+
+
+def quadruplets_from_pairs(nodes, pairs):
+    """
+    Find all connected quadruplets from a list of connected pairs.
+    Currently uses a naive algorithm, will be optimised if it turns out to be a performance bottleneck.
+
+    :param nodes: List of node names
+    :param pairs: List of node connectivity pairs
+    :return: List of node connectivity quadruplets
+    """
+    quadruplets = []
+
+    for node1 in nodes:
+        for node2 in nodes:
+            pair1 = (node1, node2)
+            if pair1 not in pairs and pair1[::-1] not in pairs:
+                continue
+
+            for node3 in nodes:
+                if node3 == node1:
+                    continue
+                pair2 = (node2, node3)
+                if pair2 not in pairs and pair2[::-1] not in pairs:
+                    continue
+
+                for node4 in nodes:
+                    if node4 == node2 or node4 == node1:
+                        continue
+                    pair3 = (node3, node4)
+                    if pair3 not in pairs and pair3[::-1] not in pairs:
+                        continue
+
+                    quadruplet = (node1, node2, node3, node4)
+                    if quadruplet not in quadruplets and quadruplet[::-1] not in quadruplets:
+                        quadruplets.append(quadruplet)
+
+    return quadruplets
+
+
 def stat_moments(vals, ignore_nan=True):
     """
     Return statistical (population) moments of data provided.
@@ -69,14 +154,15 @@ def r_squared(ref, fit):
         return 0
 
 
-def gaussian(x, mean=0, sdev=1, amplitude=1):
+def gaussian(xs, mean=0, sdev=1, amplitude=1):
     """
     Return y values from a Gaussian/normal distribution with provided mean, standard deviation and amplitude at x coordinates in vector x.
 
-    :param x: X values at which to calculate y values
+    :param xs: X values at which to calculate y values
     :param mean: Mean of Gaussian distribution
     :param sdev: Standard devaition of Gaussian distribution
     :param amplitude: Amplitude of Gaussian distribuion
     :return: Y values of Gaussian distribution at X values in x
     """
-    pass
+    gaussian_single = lambda x: (amplitude / (sdev * np.sqrt(2*np.pi))) * np.exp((-(x-mean)**2) / (2*sdev*sdev))
+    return map(gaussian_single, xs)
--- test/test_util.py
+++ test/test_util.py
@@ -2,10 +2,38 @@ import unittest
 
 import numpy as np
 
-from pycgtool.util import stat_moments, sliding, r_squared
+from pycgtool.util import stat_moments, sliding
+from pycgtool.util import r_squared, gaussian
+from pycgtool.util import triplets_from_pairs, tuple_equivalent
+from pycgtool.util import quadruplets_from_pairs
 
 
 class UtilTest(unittest.TestCase):
+    def test_tuple_equivalent(self):
+        t1 = (0, 1, 2)
+        t2 = (2, 1, 0)
+        self.assertTrue(tuple_equivalent(t1, t2))
+        t2 = (2, 1, 3)
+        self.assertFalse(tuple_equivalent(t1, t2))
+
+    def test_triplets_from_pairs(self):
+        nodes = [0, 1, 2, 3]
+        pairs = [(0, 1), (1, 2), (2, 3)]
+        result = [(0, 1, 2), (1, 2, 3)]
+        self.assertEqual(result, triplets_from_pairs(nodes, pairs))
+        pairs = [(0, 1), (1, 2), (2, 3), (3, 0)]
+        result = [(0, 1, 2), (0, 3, 2), (1, 0, 3), (1, 2, 3)]
+        self.assertEqual(result, triplets_from_pairs(nodes, pairs))
+
+    def test_quadruplets_from_pairs(self):
+        nodes = [0, 1, 2, 3]
+        pairs = [(0, 1), (1, 2), (2, 3)]
+        result = [(0, 1, 2, 3)]
+        self.assertEqual(result, quadruplets_from_pairs(nodes, pairs))
+        pairs = [(0, 1), (1, 2), (2, 3), (3, 0)]
+        result = [(0, 1, 2, 3), (0, 3, 2, 1), (1, 0, 3, 2), (2, 1, 0, 3)]
+        self.assertEqual(result, quadruplets_from_pairs(nodes, pairs))
+
     def test_stat_moments(self):
         t1 = [3, 3, 3, 3, 3]
         t2 = [1, 2, 3, 4, 5]
