Fix off-by-one error in reading frames Output GRO first again

pycgtool/frame.py
@@ -119,7 +119,6 @@ class FrameReader(metaclass=abc.ABCMeta):
 
     def initialise_frame(self, frame):
         self._initialise_frame(frame)
-        self.read_next(frame)
 
     def read_next(self, frame):
         result = self.read_frame_number(self._frame_number, frame)
@@ -216,7 +215,6 @@ class FrameReaderSimpleTraj(FrameReader):
 
             line = gro.readline()
             frame.box = np.array([float(x) for x in line.split()[0:3]], dtype=np.float32)
-            frame.number += 1
 
     def _read_frame_number(self, number):
         """
@@ -272,23 +270,31 @@ class FrameReaderMDTraj(FrameReader):
 
         :param filename: Filename of GROMACS GRO to read
         """
+        try:
+            import mdtraj
+        except ImportError as e:
+            if "scipy" in e.msg:
+                e.msg = "The MDTraj FrameReader also requires Scipy"
+            else:
+                e.msg = "The MDTraj FrameReader requires the module MDTraj (and probably Scipy)"
+            raise
         logger.warning("WARNING: Using MDTraj which renames solvent molecules")
+        top = mdtraj.load(self._topname)
 
         frame.name = ""
-        self.num_atoms = self._traj.n_atoms
-        frame.natoms = self._traj.n_atoms
+        self.num_atoms = top.n_atoms
+        frame.natoms = top.n_atoms
 
-        for residue in self._traj.topology.residues:
+        for residue in top.topology.residues:
             frame.residues.append(Residue(name=residue.name,
                                           num=residue.resSeq))
 
-        for atom in self._traj.topology.atoms:
+        for atom in top.topology.atoms:
             new_atom = Atom(name=atom.name, num=atom.serial,
-                            coords=self._traj.xyz[0][atom.index])
+                            coords=top.xyz[0][atom.index])
             frame.residues[atom.residue.index].add_atom(new_atom)
 
-        frame.box = self._traj.unitcell_lengths[0]
-        frame.number = 1
+        frame.box = top.unitcell_lengths[0]
 
     def _read_frame_number(self, number):
         """
@@ -320,9 +326,6 @@ class Frame:
         self._xtc_buffer = None
 
         if gro is not None:
-            # self._parse_gro(gro)
-            self.numframes += 1
-
             open_xtc = {"simpletraj": FrameReaderSimpleTraj,
                         "mdtraj":     FrameReaderMDTraj}
             try:

test/test_frame.py
@@ -90,7 +90,7 @@ class FrameTest(unittest.TestCase):
     def test_frame_read_xtc_simpletraj_numframes(self):
         frame = Frame(gro="test/data/water.gro", xtc="test/data/water.xtc",
                       xtc_reader="simpletraj")
-        self.assertEqual(12, frame.numframes)
+        self.assertEqual(11, frame.numframes)
 
     @unittest.skipIf(not mdtraj_present, "MDTRAJ or Scipy not present")
     def test_frame_read_xtc_mdtraj_numframes(self):
@@ -98,18 +98,18 @@ class FrameTest(unittest.TestCase):
         frame = Frame(gro="test/data/water.gro", xtc="test/data/water.xtc",
                       xtc_reader="mdtraj")
         logging.disable(logging.NOTSET)
-        self.assertEqual(12, frame.numframes)
+        self.assertEqual(11, frame.numframes)
 
     def test_frame_simpletraj_read_xtc(self):
         frame = Frame(gro="test/data/water.gro", xtc="test/data/water.xtc",
                       xtc_reader="simpletraj")
         self.assertEqual(663, frame.natoms)
         # These are the coordinates from the gro file
-        # np.testing.assert_allclose(np.array([0.696, 1.33, 1.211]),
-        #                            frame.residues[0].atoms[0].coords)
-        # np.testing.assert_allclose(np.array([1.89868, 1.89868, 1.89868]),
-        #                            frame.box)
-        # frame.next_frame()
+        np.testing.assert_allclose(np.array([0.696, 1.33, 1.211]),
+                                   frame.residues[0].atoms[0].coords)
+        np.testing.assert_allclose(np.array([1.89868, 1.89868, 1.89868]),
+                                   frame.box)
+        frame.next_frame()
 
         # These coordinates are from the xtc file
         np.testing.assert_allclose(np.array([1.176, 1.152, 1.586]),
@@ -132,11 +132,11 @@ class FrameTest(unittest.TestCase):
         self.assertEqual(663, frame.natoms)
 
         # These are the coordinates from the gro file
-        # np.testing.assert_allclose(np.array([0.696, 1.33, 1.211]),
-        #                            frame.residues[0].atoms[0].coords)
-        # np.testing.assert_allclose(np.array([1.89868, 1.89868, 1.89868]),
-        #                            frame.box)
-        # frame.next_frame()
+        np.testing.assert_allclose(np.array([0.696, 1.33, 1.211]),
+                                   frame.residues[0].atoms[0].coords)
+        np.testing.assert_allclose(np.array([1.89868, 1.89868, 1.89868]),
+                                   frame.box)
+        frame.next_frame()
 
         # These coordinates are from the xtc file
         np.testing.assert_allclose(np.array([1.176, 1.152, 1.586]),