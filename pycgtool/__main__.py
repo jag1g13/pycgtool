4c26d2a008137cc2ce102b2a8434b98634314ec7
James Graham <j.graham@soton.ac.uk>
2020-08-30 18:24:08 +0100

    build: Setup Poetry entrypoint script

--- pycgtool/__main__.py
+++ pycgtool/__main__.py
@@ -1,51 +1,173 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import sys
 
-try:
-    from pycgtool.pycgtool import main, map_only
-    from pycgtool.interface import Options
-    from pycgtool.functionalforms import FunctionalForms
-except SyntaxError:
-    raise RuntimeError("PyCGTOOL requires Python 3.2 or greater")
+from .frame import Frame
+from .mapping import Mapping
+from .bondset import BondSet
+from .forcefield import ForceField
+from .functionalforms import FunctionalForms
+from .interface import Options, Progress
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Perform coarse-grain mapping of atomistic trajectory")
+logger = logging.getLogger(__name__)
+
+
+def full_run(args, config):
+    """
+    Main function of the program PyCGTOOL.
+
+    Performs the complete AA->CG mapping and outputs a files dependent on given input.
+
+    :param args: Arguments from argparse
+    :param config: Configuration dictionary
+    """
+    frame = Frame(gro=args.gro,
+                  xtc=args.xtc,
+                  itp=args.itp,
+                  frame_start=args.begin)
+
+    if args.bnd:
+        logger.info("Bond measurements will be made")
+        bonds = BondSet(args.bnd, config)
+
+    else:
+        logger.info("Bond measurements will not be made")
+
+    if args.map:
+        logger.info("Mapping will be performed")
+        mapping = Mapping(args.map, config, itp=args.itp)
+        cgframe = mapping.apply(frame)
+        cgframe.output(config.output_name + ".gro", format=config.output)
+
+    else:
+        logger.info("Mapping will not be performed")
+        cgframe = frame
+
+    # Only measure bonds from GRO frame if no XTC is provided
+    # Allows the user to get a topology from a single snapshot
+    if args.bnd and args.xtc is None:
+        bonds.apply(cgframe)
+
+    # Main loop - perform mapping and measurement on every frame in XTC
+    def main_loop():
+        nonlocal cgframe
+        if not frame.next_frame():
+            return False
+
+        if args.map:
+            cgframe = mapping.apply(frame, cgframe=cgframe)
+
+            if config.output_xtc:
+                cgframe.write_xtc(config.output_name + ".xtc")
+
+        else:
+            cgframe = frame
+
+        if args.bnd:
+            bonds.apply(cgframe)
+
+        return True
+
+    numframes = frame.numframes - args.begin if args.end == -1 else args.end - args.begin
+    logger.info("Beginning analysis of {0} frames".format(numframes))
+    Progress(numframes, dowhile=main_loop, quiet=args.quiet).run()
+
+    if args.bnd:
+        if args.map:
+            logger.info("Beginning Boltzmann inversion")
+            bonds.boltzmann_invert(progress=(not args.quiet))
+
+            if config.output_forcefield:
+                logger.info("Creating GROMACS forcefield directory")
+                ForceField(config.output_name).write(config.output_name,
+                                                     mapping, bonds)
+                logger.info("GROMACS forcefield directory created")
+
+            else:
+                bonds.write_itp(config.output_name + ".itp", mapping=mapping)
+
+        if config.dump_measurements:
+            logger.info("Dumping bond measurements to file")
+            bonds.dump_values(config.dump_n_values)
+
+
+def map_only(args, config):
+    """
+    Perform AA->CG mapping and output coordinate file.
+
+    :param args: Program arguments
+    :param config: Object containing run options
+    """
+    frame = Frame(gro=args.gro, xtc=args.xtc)
+    mapping = Mapping(args.map, config)
+    cgframe = mapping.apply(frame)
+    cgframe.output(config.output_name + ".gro", format=config.output)
+
+    if args.xtc and (config.output_xtc or args.outputxtc):
+        # Main loop - perform mapping and measurement on every frame in XTC
+        def main_loop():
+            nonlocal cgframe
+            if not frame.next_frame():
+                return False
+
+            cgframe = mapping.apply(frame, cgframe=cgframe)
+            cgframe.write_xtc(config.output_name + ".xtc")
+            return True
+
+        numframes = frame.numframes - args.begin if args.end == -1 else args.end - args.begin
+        logger.info("Beginning analysis of {0} frames".format(numframes))
+        its = Progress(numframes, dowhile=main_loop, quiet=args.quiet).run()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Perform coarse-grain mapping of atomistic trajectory")
     input_files = parser.add_argument_group("Input files")
+
+    # yapf: disable
     input_files.add_argument('-g', '--gro', type=str, required=True, help="GROMACS GRO file")
     input_files.add_argument('-m', '--map', type=str, help="Mapping file")
     input_files.add_argument('-x', '--xtc', type=str, help="GROMACS XTC file")
     input_files.add_argument('-b', '--bnd', type=str, help="Bonds file")
     input_files.add_argument('-i', '--itp', type=str, help="GROMACS ITP file")
+    # yapf: enable
 
-    parser.add_argument('--advanced', default=False, action='store_true', help="Show advanced options menu")
-    parser.add_argument('--outputxtc', default=False, action='store_true', help="Output a pseudo-CG trajectory")
-    parser.add_argument('--quiet', default=False, action='store_true', help="Hide progress bars")
-    input_files.add_argument('--begin', type=int, default=0, help="Frame number to begin")
-    input_files.add_argument('--end', type=int, default=-1, help="Frame number to end")
+    parser.add_argument('--advanced',
+                        default=False,
+                        action='store_true',
+                        help="Show advanced options menu")
+    parser.add_argument('--outputxtc',
+                        default=False,
+                        action='store_true',
+                        help="Output a pseudo-CG trajectory")
+    parser.add_argument('--quiet',
+                        default=False,
+                        action='store_true',
+                        help="Hide progress bars")
+    input_files.add_argument('--begin',
+                             type=int,
+                             default=0,
+                             help="Frame number to begin")
+    input_files.add_argument('--end',
+                             type=int,
+                             default=-1,
+                             help="Frame number to end")
 
     func_forms = FunctionalForms()
 
     args = parser.parse_args()
-    config = Options([
-        ("output_name", "out"),
-        ("output", "gro"),
-        ("output_xtc", args.outputxtc),
-        ("map_only", not bool(args.bnd)),
-        ("map_center", "geom"),
-        ("constr_threshold", 100000),
-        ("dump_measurements", bool(args.bnd) and not bool(args.map)),
-        ("dump_n_values", 10000),
-        ("output_forcefield", False),
-        ("temperature", 310),
-        ("default_fc", False),
-        ("generate_angles", True),
-        ("generate_dihedrals", False),
-        ("length_form", "harmonic"),
-        ("angle_form", "cosharmonic"),
-        ("dihedral_form", "harmonic")
-    ], args)
+    config = Options(
+        [("output_name", "out"), ("output", "gro"),
+         ("output_xtc", args.outputxtc), ("map_only", not bool(args.bnd)),
+         ("map_center", "geom"), ("constr_threshold", 100000),
+         ("dump_measurements", bool(args.bnd) and not bool(args.map)),
+         ("dump_n_values", 10000), ("output_forcefield", False),
+         ("temperature", 310), ("default_fc", False),
+         ("generate_angles", True), ("generate_dihedrals", False),
+         ("length_form", "harmonic"), ("angle_form", "cosharmonic"),
+         ("dihedral_form", "harmonic")], args)
 
     if not args.map and not args.bnd:
         parser.error("One or both of -m and -b is required.")
@@ -53,13 +175,20 @@ if __name__ == "__main__":
     if args.advanced:
         try:
             config.interactive()
+
         except KeyboardInterrupt:
             sys.exit(0)
+
     else:
         print("Using GRO: {0}".format(args.gro))
         print("Using XTC: {0}".format(args.xtc))
 
     if config.map_only:
         map_only(args, config)
+
     else:
-        main(args, config)
+        full_run(args, config)
+
+
+if __name__ == "__main__":
+    main()
--- test/test_pycgtool.py
+++ test/test_pycgtool.py
@@ -10,20 +10,21 @@ from simpletraj.trajectory import XtcTrajectory
 try:
     import mdtraj
     mdtraj_present = True
+
 except ImportError:
     mdtraj_present = False
 
 from pycgtool.interface import Options
 from pycgtool.util import cmp_whitespace_float
-from pycgtool.pycgtool import main, map_only
+from pycgtool.__main__ import map_only
 
 
 class Args:
     def __init__(self, name, map=True, bnd=True):
-        self.gro = os.path.join("test/data", name+".gro")
-        self.xtc = os.path.join("test/data", name+".xtc")
-        self.map = os.path.join("test/data", name+".map") if map else None
-        self.bnd = os.path.join("test/data", name+".bnd") if bnd else None
+        self.gro = os.path.join("test/data", name + ".gro")
+        self.xtc = os.path.join("test/data", name + ".xtc")
+        self.map = os.path.join("test/data", name + ".map") if map else None
+        self.bnd = os.path.join("test/data", name + ".bnd") if bnd else None
         self.begin = 0
         self.end = -1
         self.quiet = True
@@ -42,11 +43,13 @@ class PycgtoolTest(unittest.TestCase):
                       ("temperature", 310),
                       ("default_fc", False),
                       ("generate_angles", True),
-                      ("generate_dihedrals", False)])
+                      ("generate_dihedrals", False)])  # yapf: disable
 
     def test_run_help(self):
-        path = os.path.dirname(os.path.dirname(__file__))
-        self.assertEqual(0, subprocess.check_call([os.path.join(path, "pycgtool.py"), "-h"], stdout=subprocess.PIPE))
+        self.assertEqual(
+            0,
+            subprocess.check_call(["python", "-m", "pycgtool", "-h"],
+                                  stdout=subprocess.PIPE))
 
     @unittest.skipIf(not mdtraj_present, "MDTRAJ or Scipy not present")
     def test_map_only(self):
@@ -61,19 +64,31 @@ class PycgtoolTest(unittest.TestCase):
         for i in range(xtc_ref.numframes):
             xtc.get_frame(i)
             xtc_ref.get_frame(i)
-            np.testing.assert_array_almost_equal(xtc_ref.box, xtc.box, decimal=3)
+            np.testing.assert_array_almost_equal(xtc_ref.box,
+                                                 xtc.box,
+                                                 decimal=3)
             np.testing.assert_array_almost_equal(xtc_ref.x, xtc.x, decimal=3)
 
     def test_full(self):
-        path = os.path.dirname(os.path.dirname(__file__))
-        self.assertEqual(0, subprocess.check_call([os.path.join(path, "pycgtool.py"),
-                                                   "-g", "test/data/sugar.gro",
-                                                   "-x", "test/data/sugar.xtc",
-                                                   "-m", "test/data/sugar_only.map",
-                                                   "-b", "test/data/sugar.bnd",
-                                                   ], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
-        self.assertTrue(cmp_whitespace_float("out.itp", "test/data/sugar_out.itp", float_rel_error=0.001))
-        self.assertTrue(cmp_whitespace_float("out.gro", "test/data/sugar_out.gro", float_rel_error=0.001))
+        self.assertEqual(
+            0,
+            subprocess.check_call([
+                "python", "-m", "pycgtool",
+                "-g", "test/data/sugar.gro",
+                "-x", "test/data/sugar.xtc",
+                "-m", "test/data/sugar_only.map",
+                "-b", "test/data/sugar.bnd",
+            ]))  # yapf: disable
+
+        self.assertTrue(
+            cmp_whitespace_float("out.itp",
+                                 "test/data/sugar_out.itp",
+                                 float_rel_error=0.001))
+        self.assertTrue(
+            cmp_whitespace_float("out.gro",
+                                 "test/data/sugar_out.gro",
+                                 float_rel_error=0.001))
+
     # TODO more tests
 
 
