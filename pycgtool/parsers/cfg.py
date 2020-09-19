a5f5d1301f1a8c8a5cf84b8a6bc4439a2191c5e0
James Graham <j.graham@soton.ac.uk>
2020-09-19 15:47:10 +0100

    refactor: Reduce duplication in custom parsers

--- pycgtool/parsers/cfg.py
+++ pycgtool/parsers/cfg.py
@@ -1,66 +1,77 @@
-"""
-Module containing classes used to parse custom CFG file format.
+"""Module containing classes used to parse custom CFG file format.
 
 Format is based upon GROMACS .itp files but does not support nesting of sections.
 """
-import os
 
 import collections
+import contextlib
+import pathlib
+import typing
 
 
 class DuplicateSectionError(KeyError):
-    """
-    Exception used to indicate that a section has appeared twice in a file.
-    """
+    """Exception used to indicate that a section has appeared twice in a file."""
     def __init__(self, section, filename):
-        msg = "Section '{0}' appears twice in file '{1}'.".format(section, filename)
-        super(DuplicateSectionError, self).__init__(msg)
+        msg = f"Section '{section}' appears twice in file '{filename}'."
+        super().__init__(msg)
 
 
 class NoSectionError(KeyError):
-    """
-    Exception used to indicate that a file contains no sections.
-    """
+    """Exception used to indicate that a file contains no sections."""
     def __init__(self, filename):
-        msg = "File '{0}' contains no '[]' section headers.".format(filename)
-        super(NoSectionError, self).__init__(msg)
+        msg = f"File '{filename}' contains no '[]' section headers."
+        super().__init__(msg)
 
 
-class CFG(collections.OrderedDict):
-    """
-    Class representing a CFG file.
+class CFG(collections.OrderedDict, contextlib.AbstractContextManager):
+    """Class representing a CFG file.
 
     Contains a dictionary of Sections.
     """
-    __slots__ = ["filename"]
-
-    def __init__(self, filename=None):
-        """
-        Parse a config file and extract Sections.
+    def __init__(self,
+                 filename: typing.Optional[typing.Union[str,
+                                                        pathlib.Path]] = None):
+        """Parse a config file and extract Sections.
 
         :param filename: Name of file to read
         :return: Instance of CFG
         """
-        super(CFG, self).__init__()
-        self.filename = filename
+        super().__init__()
+        self.filename = None
 
+        if filename is not None:
+            self.filename = pathlib.Path(filename)
+            self._read_file()
+
+    def _read_line(self, line: str) -> typing.Optional[str]:
+        # Strip comments
+        line = line.split(';')[0].strip()
+        if not line:
+            return None
+
+        # Handle include directive
+        if line.startswith('#include'):
+            include_file = line.split()[1].strip('"')
+            other = type(self)(self.filename.parent.joinpath(include_file))
+            self.update(other)
+            return None
+
+        return line
+
+    def _read_file(self) -> None:
         with open(self.filename) as f:
             curr_section = None
+
             for line in f:
-                line = line.strip()
-                if not line or line.startswith(";"):
+                line = self._read_line(line)
+                if line is None:
                     continue
 
-                elif line.startswith("#include"):
-                    cfg2 = CFG(os.path.join(os.path.dirname(self.filename),
-                                            line.split()[1]))
-                    self.update(cfg2)
-                    continue
-
-                elif line.startswith("["):
+                if line.startswith("["):
                     curr_section = line.strip("[ ]")
                     if curr_section in self:
-                        raise DuplicateSectionError(curr_section, self.filename)
+                        raise DuplicateSectionError(curr_section,
+                                                    self.filename)
                     self[curr_section] = []
                     continue
 
@@ -69,9 +80,3 @@ class CFG(collections.OrderedDict):
                     self[curr_section].append(toks)
                 except KeyError as e:
                     raise NoSectionError(self.filename) from e
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        pass
